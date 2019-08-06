92c318e3cb494d7f15743513484ae105ee69d1f7
Apoorv Singh <apoorv.singh15@gmail.com>
2019-08-06 19:11:25 +0530

    added style changes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 import Close from "@material-ui/icons/Close";
+import Grade from "@material-ui/icons/Grade";
 const Note = props => {
   return (
     <div
       className="effect5"
-      style={
-        props.importance === "important"
-          ? {
-              padding: 10,
-              margin: 15,
-              boxShadow:
-                "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
-            }
-          : props.importance === "medium"
-          ? {
-              padding: 10,
-              margin: 15,
-              boxShadow:
-                "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
-            }
-          : {
-              padding: 10,
-              margin: 15,
-              boxShadow:
-                "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
-            }
-      }
+      style={{
+        padding: 10,
+        margin: 15,
+        boxShadow: "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
+      }}
     >
+      <Grade
+        style={
+          props.importance === "important"
+            ? {
+                marginRight: 10,
+                marginBottom: "-6px",
+                cursor: "pointer",
+                color: "red"
+              }
+            : props.importance === "medium"
+            ? {
+                marginRight: 10,
+                marginBottom: "-6px",
+                cursor: "pointer",
+                color: "yellow"
+              }
+            : {
+                marginRight: 10,
+                marginBottom: "-6px",
+                cursor: "pointer",
+                color: "green"
+              }
+        }
+      />
       {props.content}
       <Close
         style={{ marginLeft: 10, marginBottom: "-6px", cursor: "pointer" }}
